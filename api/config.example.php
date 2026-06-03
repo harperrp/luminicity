371940ad61151cf2ce15404c4d4c69a46037ac19
harperrp <124433217+harperrp@users.noreply.github.com>
2026-06-03 18:09:03 -0300

    att

--- api/config.example.php
+++ api/config.example.php
@@ -3,8 +3,8 @@ return [
   'db_host' => 'localhost',
   'db_port' => '3306',
   'db_name' => 'radgov_city_light',
-  'db_user' => 'usuario_do_banco',
-  'db_pass' => 'senha_do_banco',
+  'db_user' => 'radgovilumini',
+  'db_pass' => 'B4hb4hb4h@@',
   'session_name' => 'luminicity_admin',
   'allowed_origins' => [
     'http://localhost:8080',
